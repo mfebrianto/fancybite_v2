2d3c90fa44ccc297e3366c577a3a3d67e411ab0f
michael febrianto <michaelfebrianto@gmail.com>
2015-12-09 20:47:54 +1100

    dashboard-css : include slider for awesome menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require turbolinks
 //= require jquery.bxslider.min
 //= require features
+//= require awesome_products
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
---
+++ app/assets/javascripts/awesome_products.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.awesome-products').bxSlider({
+        minSlides: 3,
+        maxSlides: 3,
+        slideWidth: 720,
+        slideMargin: 10
+    });
+});
