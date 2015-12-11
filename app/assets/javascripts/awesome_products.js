6b30eae447e6fe0a1bb10b31bd9ff18dbccee496
michael febrianto <michaelfebrianto@gmail.com>
2015-12-11 20:02:36 +1100

    dashboard-css : responsive slider

--- app/assets/javascripts/awesome_products.js
+++ app/assets/javascripts/awesome_products.js
@@ -1,8 +1,43 @@
+var slider;
+function sliderOptions(width){
+
+    var options;
+    if(width>765) {
+        options = {
+            minSlides: 3,
+            maxSlides: 3,
+            slideWidth: 700,
+            slideMargin: 21,
+            responsive: false
+        }
+    }
+    else if(width>555&&width<=765) {
+        options = {
+            minSlides: 3,
+            maxSlides: 3,
+            slideWidth: 700,
+            slideMargin: 21,
+            responsive: false
+        }
+    }
+    else {
+        options = {
+            minSlides: 1,
+            maxSlides: 1,
+            slideWidth:0,
+            moveSlides:1,
+            responsive: false
+        }
+    }
+
+    return options;
+}
+
 $(document).ready(function(){
-    $('.awesome-products').bxSlider({
-        minSlides: 3,
-        maxSlides: 3,
-        slideWidth: 720,
-        slideMargin: 10
-    });
+    slider = $('.awesome-products').bxSlider(sliderOptions($( window ).width()));
+});
+
+$(window).on('resize', function(){
+    slider.destroySlider();
+    slider = $('.awesome-products').bxSlider(sliderOptions($( window ).width()));
 });
