5a15bde0e59d3c4187804f831d780f20c4a56359
michael febrianto <michaelfebrianto@gmail.com>
2016-01-15 16:45:50 +1100

    CRUD operation for menus is done

---
+++ app/controllers/admin/menus_controller.rb
@@ -0,0 +1,49 @@
+class Admin::MenusController < Admin::AdminController
+
+  def index
+    @all_menu = Menu.all
+  end
+
+  def new
+    @menu = Menu.new
+  end
+
+  def edit
+    @menu = Menu.find(param_id['id'])
+  end
+
+  def update
+    @menu = Menu.find(param_id['id'])
+    @menu.update_attributes(menu_params)
+    render 'show'
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    @menu.save
+    render 'show'
+  end
+
+  def show
+    @menu = Menu.find(param_id['id'])
+  end
+
+  def destroy
+    @menu = Menu.find(param_id['id'])
+    if @menu.delete
+      @all_menu = Menu.all
+      render 'index'
+    end
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:name, :description, :price)
+  end
+
+  def param_id
+    params.permit(:id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :dashboard
-    resources :menu
+    resources :menus
   end
 
   # You can have the root of your site routed with "root"
