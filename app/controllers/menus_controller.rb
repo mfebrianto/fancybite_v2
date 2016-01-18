26c789e7ebce5e8c464a0a03208ed041be31ef22
michael febrianto <michaelfebrianto@gmail.com>
2016-01-19 09:21:04 +1100

    update menu

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,7 +1,27 @@
 class MenusController < ApplicationController
 
   def index
+    @foods_array_of_3 = convert_array_to_dimension(Menu.all, 3)
+  end
 
+  private
+
+  def convert_array_to_dimension(original_array, new_dimension)
+    index = 0
+    result_array = []
+    temp_array = []
+    original_array.each do |array_member|
+      if index == new_dimension
+        index = 0
+        result_array << temp_array
+        temp_array = []
+      end
+
+      temp_array << array_member
+      Rails.logger.info ">>>>>>>temp_array>>>>#{temp_array}"
+      index += 1
+    end
+    result_array
   end
 
 end
