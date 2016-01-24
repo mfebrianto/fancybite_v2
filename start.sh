140a637161b5029b8daeb324ce6594a6a8d91dcf
michael febrianto <michaelfebrianto@gmail.com>
2016-01-24 20:41:18 +1100

    make sure use production when start the server

--- start.sh
+++ start.sh
@@ -6,4 +6,5 @@ RAILS_ENV=production rake db:create
 RAILS_ENV=production rake db:migrate
 RAILS_ENV=production rake db:seed
 bundle exec rake log:clear
-rails s
+#rails s
+RAILS_ENV=production rails server -p 3000 -b '0.0.0.0'
