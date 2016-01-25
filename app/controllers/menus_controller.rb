1b4075308787a56261df62dfd2b3432126df54c2
michael febrianto <michaelfebrianto@gmail.com>
2016-01-25 16:56:22 +1100

    fix issue when showing the menu

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -11,22 +11,22 @@ class MenusController < ApplicationController
   private
 
   def convert_array_to_dimension(original_array, new_dimension)
-    index = 0
-    order = 0
+    index = 1
+    order = 1
     result_array = []
     temp_array = []
     original_array.each do |array_member|
       temp_array << array_member
 
-      if index == (new_dimension-1) || (order == original_array.count - 1)
+      if index == new_dimension || (order == original_array.count)
         index = 0
         result_array << temp_array
         temp_array = []
       end
-      index += 1
+
       order += 1
+      index += 1
     end
     result_array
   end
-
 end
