baebead3daeaa6d0f8a7e0ce9cc7cccf87487929
michael febrianto <michaelfebrianto@gmail.com>
2016-02-02 09:21:39 +1100

    201601-10 : add instagram fetcher

--- app/interactors/instagram.rb
+++ app/interactors/instagram.rb
@@ -2,11 +2,27 @@ class Instagram
 
   INSTAGRAM_URI="https://api.instagram.com/v1/tags/%s?access_token=%s"
 
-  def self.number
-    tag = Promo.instagram_tag
-    uri = INSTAGRAM_URI % [tag, Settings.instagram.access_token]
-    response = HTTParty.get(uri)
-    response
+  attr_accessor :instagram_tag
+
+  def initialize
+    @instagram_tag = Promo.instagram_tag
+    number
   end
 
+  def number
+    uri = INSTAGRAM_URI % [@instagram_tag, Settings.instagram.access_token]
+    parse_response(HTTParty.get(uri))
+  end
+
+  def parse_response(response)
+    if response.present? &&
+        response['meta']['code'] == 200
+      current_no_of_tag = InstagramTagNumber.find(@instagram_tag)
+      if current_no_of_tag < response['data']['media_count']
+        InstagramTagNumber.create(tag_number: response['data']['media_count'],
+                                  tag: response['data']['name'],
+                                  fetch_date: Time.now.to_s)
+      end
+    end
+  end
 end
