8d336c0cc1f04098f8e5b63137a6cd6d937ef46c
michael febrianto <michaelfebrianto@gmail.com>
2016-03-02 09:13:21 +1100

    201603-03 : login and logout works

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,17 @@
 module ApplicationHelper
 
   SHOP = 1
+  LOGIN = 2
 
   def customer_buy_active?
     !FeatureControl.find(SHOP).disabled?
+  end
+
+  def login_active?
+    !FeatureControl.find(LOGIN).disabled?
+  end
+
+  def show_login_link?
+    login_active? && !current_user.present?
   end
 end
