25e8bfa17554794b41f7b28347aa8c0499713cc5
michael febrianto <michaelfebrianto@gmail.com>
2016-03-03 18:56:36 +1100

    201603-06 : move shopping cart icon to floating header

--- app/assets/javascripts/shop/shopping_cart.js
+++ app/assets/javascripts/shop/shopping_cart.js
@@ -1,6 +1,9 @@
 shop.shopping_cart = {
     addItem: function(){
         console.log('item added');
+        // call backend to add item to database
+        // if success return total
+        // set the total in the view
     }
 }
 
