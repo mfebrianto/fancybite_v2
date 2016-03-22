b7dd742e2217875bd0c99a52986ce4f33b1062bd
michael febrianto <michaelfebrianto@gmail.com>
2016-03-23 08:52:57 +1100

    201603-08 : delivery time update done

--- app/controllers/admin/delivery_time_schedule_controller.rb
+++ app/controllers/admin/delivery_time_schedule_controller.rb
@@ -1,26 +1,26 @@
 class Admin::DeliveryTimeScheduleController < Admin::AdminController
 
   def index
-    # @delivery_time_templates = DeliveryTimeTemplate.all
+    @delivery_time_schedules = DeliveryTimeSchedule.all
   end
 
   def edit
-    # @delivery_time_templates = DeliveryTimeTemplate.all
+    @delivery_time_schedules = DeliveryTimeSchedule.all
   end
 
   def update_all
-    # params['delivery_time_template'].each do |id, val|
-    #   delivery_time_template = DeliveryTimeTemplate.find(id)
-    #   delivery_time_template.update_attributes(form_params(val))
-    # end
-    # redirect_to action: 'index'
+    params['delivery_time_schedule'].each do |id, val|
+      delivery_time_template = DeliveryTimeSchedule.find(id)
+      delivery_time_template.update_attributes(form_params(val))
+    end
+    redirect_to action: 'index'
   end
 
   private
 
   def form_params(val)
-    # { morning: val['morning'],
-    #   afternoon: val['afternoon'],
-    #   evening: val['evening'] }
+    { morning: val['morning'],
+      afternoon: val['afternoon'],
+      evening: val['evening'] }
   end
 end
