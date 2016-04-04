0efd31b76b6f289502573953dcc6f345670ed402
michael febrianto <michaelfebrianto@gmail.com>
2016-04-04 23:28:27 +1000

    201604-02 : upload image with serialized form done

--- app/assets/javascripts/admin/social.js
+++ app/assets/javascripts/admin/social.js
@@ -1,16 +1,30 @@
 fancybite.admin.social = {
     init: function() {
+        console.log('admin social init');
         fancybite.admin.social.listeners();
     },
     listeners: function() {
         $('#upload_social_photo').click(function(e){
             e.preventDefault();
+            console.log('button clicked');
+
+            var formData = new FormData();
+            formData.append('picture', $('#picture')[0].files[0]);
+
+            var other_data = $('#social_image_form').serializeArray();
+            $.each(other_data,function(key,input){
+                formData.append(input.name,input.value);
+            });
+
             $.ajax({
                 type: 'POST',
                 url: '/admin/social_images',
-                data: $('#social_image_form').serialize(),
-                success: function(){
-                    console.log('photo uploaded successfully');
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function(data){
+                    $('#new-social-image-wrapper').replaceWith(data);
+                    fancybite.admin.social.init();
                 }
             });
         });
--- app/controllers/admin/social_images_controller.rb
+++ app/controllers/admin/social_images_controller.rb
@@ -1,7 +1,21 @@
 class Admin::SocialImagesController < Admin::AdminController
 
   def create
+    social = params[:social_id].blank? ? Social.new : Social.find(params[:social_id])
 
+    social.social_images.build(picture: params[:picture])
+    if social.save
+      @social_id = social.id
+      show_all
+      render partial: 'admin/social_images/new'
+    end
+  end
+
+  private
+
+
+  def show_all
+    @social_images = SocialImage
   end
 
 end
