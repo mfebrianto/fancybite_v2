2b988df114d0de6cbd40937980ae9ed56562f584
michael febrianto <michaelfebrianto@gmail.com>
2016-04-11 18:24:23 +1000

    201603-08 : open paypal

--- app/controllers/payments/paypal_controller.rb
+++ app/controllers/payments/paypal_controller.rb
@@ -1,14 +1,17 @@
 class Payments::PaypalController < ApplicationController
 
   def new
-    response = EXPRESS_GATEWAY.setup_purchase(YOUR_TOTAL_AMOUNT_IN_CENTS,
+    response = EXPRESS_GATEWAY.setup_purchase(300,
                                               ip: request.remote_ip,
-                                              return_url: YOUR_RETURN_URL_,
-                                              cancel_return_url: YOUR_CANCEL_RETURL_URL,
-                                              currency: "USD",
+                                              return_url: new_payments_confirmation_url,
+                                              cancel_return_url: new_checkout_url,
+                                              currency: 'AUD',
                                               allow_guest_checkout: true,
-                                              items: [{name: "Order", description: "Order description", quantity: "1", amount: AMOUNT_IN_CENTS}]
+                                              items: [{name: 'Order', description: 'Order description', quantity: '1', amount: 300}]
     )
+
+    p ">>>>>#{response.inspect}"
+
     redirect_to EXPRESS_GATEWAY.redirect_url_for(response.token)
   end
 
